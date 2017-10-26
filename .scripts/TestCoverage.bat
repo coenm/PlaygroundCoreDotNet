6fce8137772dda6167b6528428938ffa7d39a70c
Coen Munckhof <c.munckhof@gmail.com>
2017-10-26 23:23:18 +0200

    only test project

--- .scripts/TestCoverage.bat
+++ .scripts/TestCoverage.bat
@@ -2,7 +2,7 @@
 
 IF DEFINED APPVEYOR (
 
-    dotnet test -c Debug --no-build
+    dotnet test src\Playground.Calculator.Test\Playground.Calculator.Test.csproj -c Debug --no-build
     REM C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"%xunit20%\xunit.console.x86.exe" -targetargs:"src\Playground.Calculator.Test\bin\Debug\netcoreapp2.0\Playground.Calculator.Test.dll -noshadow -appveyor -xml .\xunit-results.xml" -threshold:1 -oldStyle -returntargetcode -filter:"+[Playground*]* -[Playground*.Test]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
 ) ELSE (
 	echo No coverage without appveyor.
