016cc2fb48aee51977cc13821dab51b2bee6fb3d
Coen Munckhof <c.munckhof@gmail.com>
2017-10-26 23:41:10 +0200

    improve coverage script

--- .scripts/TestCoverage.bat
+++ .scripts/TestCoverage.bat
@@ -2,8 +2,8 @@
 
 IF DEFINED APPVEYOR (
 
-    dotnet test src\Playground.Calculator.Test\Playground.Calculator.Test.csproj -c Debug --no-build --logger \"trx;LogFileName=TestResults.trx\"
-    C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"dotnet" -targetargs:"test src\Playground.Calculator.Test\Playground.Calculator.Test.csproj -c Debug --no-build --logger \"trx;LogFileName=TestResults.trx\"" -threshold:1 -oldStyle -returntargetcode -filter:"+[Playground*]* -[Playground*.Test]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
+    REM dotnet test src\Playground.Calculator.Test\Playground.Calculator.Test.csproj -c Debug --no-build --logger \"trx;LogFileName=%APPVEYOR_BUILD_FOLDER%\TestResults.trx\"
+    C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"dotnet.exe" -targetargs:"test src\Playground.Calculator.Test\Playground.Calculator.Test.csproj -c Debug --no-build --logger \"trx;%APPVEYOR_BUILD_FOLDER%\LogFileName=TestResults.trx\"" -threshold:1 -oldStyle -returntargetcode -filter:"+[Playground*]* -[Playground*.Test]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
     REM C:\ProgramData\chocolatey\lib\opencover.portable\tools\OpenCover.Console.exe -register:user -target:"%xunit20%\xunit.console.x86.exe" -targetargs:"src\Playground.Calculator.Test\bin\Debug\netcoreapp2.0\Playground.Calculator.Test.dll -noshadow -appveyor -xml .\xunit-results.xml" -threshold:1 -oldStyle -returntargetcode -filter:"+[Playground*]* -[Playground*.Test]*" -excludebyattribute:*.ExcludeFromCodeCoverage* -excludebyfile:*\*Designer.cs -hideskipped:All -mergebyhash -mergeoutput -output:.\coverage-dotnet.xml
 ) ELSE (
 	echo No coverage without appveyor.
